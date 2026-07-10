e7225c2280be088d4f4675aeea5faf65091f4c94
1vivy <1vivy@tutanota.com>
2026-07-10 04:30:41 +0000

    camera-macan: report stock OTA version
    Change-Id: Ia7d2223ca4a892e25b94e741b78d1281573c83bb

--- camera-macan.mk
+++ camera-macan.mk
@@ -6,3 +6,6 @@
 $(call inherit-product, vendor/oneplus/camera-sm8850-common/camera-sm8850-common.mk)
 
 $(call inherit-product, proprietary/vendor/oneplus/camera-macan/camera-macan-vendor.mk)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.version.ota=CPH2769_11.A.47_0470_202606060132
